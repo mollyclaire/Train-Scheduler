1a886bc755306ab4889fe408671463792dc8d50e
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-12 16:27:13 -0500

    Successfully added train name, destination, and frequency to the server and html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,50 +12,52 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
 // Create a variable to reference the database
-// var database = firebase.database();
+var database = firebase.database();
 
-// // Initial variables
-// var name = "";
-// var destination = "";
-// var time = "";
-// var frequency = "";
+// Initial variables
+var name = "";
+var destination = "";
+var time = "";
+var frequency = "";
 
-// // Click Button changes what is stored in Firebase
-// $("#click-button").on("click", function(event) {
-//     // Prevent the page from refreshing
-//     event.preventDefault();
+// Click Button changes what is stored in Firebase
+$("#click-button").on("click", function(event) {
+    // Prevent the page from refreshing
+    event.preventDefault();
 
-//     // Get inputs
-//     name = $("#input-name").val().trim();
-//     destination = $("#input-destination").val().trim();
-//     time = $("#input-time").val().trim();
-//     frequency = $("#input-frequency").val().trim();
+    // Get inputs
+    name = $("#input-name").val().trim();
+    console.log(name);
+    destination = $("#input-destination").val().trim();
+    time = $("#input-time").val().trim();
+    frequency = $("#input-frequency").val().trim();
 
-//     // Change what is saved in Firebase
-//     database.ref().push({
-//         name: name,
-//         destination: destination,
-//         time: time,
-//         frequency: frequency
-//     });
-//     });
+    // Change what is saved in Firebase
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
+    });
+    });
 
-// // Listens for changes that have been made in Firebase...
-// database.ref().on("value", function(snapshot) {
-//     console.log(snapshot.val());
-//     console.log(snapshot.val().name);
-//     console.log(snapshot.val().destination);
-//     console.log(snapshot.val().time);
-//     console.log(snapshot.val().frequency);
+// Listens for changes that have been made in Firebase and adds the data to the server...
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().time);
+    console.log(childSnapshot.val().frequency);
 
-//     // A new row is created, so the new inputs can be displayed on the page
-//     newRow = $("<tr>");
-//     newRow.append(
-//         $("<td>").text(snapshot.val().name),
-//         $("<td>").text(snapshot.val().destination),
-//         $("<td>").text(snapshot.val().time),
-//         $("<td>").text(snapshot.val().frequency)
-//     )
-//     $("#table").append(newRow);
-    
-// })
+    // A new row is created, and the new data is displayed on the page
+    var newRow = $("<tr>");
+    newRow.append(
+        $("<td>").text(childSnapshot.val().name),
+        $("<td>").text(childSnapshot.val().destination),
+        // $("<td>").text(childSnapshot.val().time),
+        $("<td>").text(childSnapshot.val().frequency)
+    )
+    $("#table").append(newRow);
+   
+
+
+})
